5a8759def3aa0adb780e6c3d379e601d61d3eeea
Kaero <kaero598@users.noreply.github.com>
2024-07-29 16:35:48 +0300

    Added tests for storage implementation

---
+++ app/storage/storage_test.go
@@ -0,0 +1,235 @@
+package storage
+
+import (
+	"testing"
+	"time"
+	"zinktray/app/message"
+)
+
+func TestAdd(t *testing.T) {
+	var storage = NewStorage()
+
+	var mboxID = "mailbox_1"
+
+	var mboxCount = storage.CountMailboxes()
+	var mboxCountExpected = 0
+
+	if mboxCount != mboxCountExpected {
+		t.Fatalf("Mailbox count is wrong: got %d, expected %d", mboxCount, mboxCountExpected)
+	}
+
+	var msgCount = storage.CountMessages(mboxID)
+	var msgCountExpected = 0
+
+	if msgCount != msgCountExpected {
+		t.Fatalf("Message count is wrong: got %d, expected %d", msgCount, msgCountExpected)
+	}
+
+	var msgExpected = &message.Message{ID: "message_1", ReceivedAt: time.Time{}}
+
+	storage.Add(msgExpected, mboxID)
+
+	mboxCount = storage.CountMailboxes()
+	mboxCountExpected = 1
+
+	if mboxCount != mboxCountExpected {
+		t.Fatalf("Mailbox count is wrong: got %d, expected %d", mboxCount, mboxCountExpected)
+	}
+
+	msgCount = storage.CountMessages(mboxID)
+	msgCountExpected = 1
+
+	if msgCount != msgCountExpected {
+		t.Fatalf("Message count is wrong: got %d, expected %d", msgCount, msgCountExpected)
+	}
+
+	var msg = storage.GetMessage(msgExpected.ID)
+
+	if msg == nil {
+		t.Fatalf("Message \"%s\" not found", msgExpected.ID)
+	}
+
+	if msg != msgExpected {
+		t.Fatalf("Message \"%s\" is not the same as the one added", msgExpected.ID)
+	}
+}
+
+func TestDeleteMailbox(t *testing.T) {
+	var storage = NewStorage()
+
+	var mboxID = "mailbox_1"
+	var msgID = "message_1"
+
+	storage.Add(&message.Message{ID: msgID, ReceivedAt: time.Time{}}, mboxID)
+	storage.Add(&message.Message{ID: "message_2", ReceivedAt: time.Time{}}, mboxID)
+
+	var mbox = storage.GetMailbox(mboxID)
+
+	if mbox == nil {
+		t.Fatalf("Mailbox \"%s\" not found", mboxID)
+	}
+
+	if mbox.ID != mboxID {
+		t.Fatalf("Mailbox ID does not match: got \"%s\", expected \"%s\"", mbox.ID, mboxID)
+	}
+
+	storage.DeleteMessage(msgID)
+
+	var msgCount = storage.CountMessages(mboxID)
+	var msgCountExpected = 1
+
+	if msgCount != msgCountExpected {
+		t.Fatalf("Message count is wrong: got %d, expected %d", msgCount, msgCountExpected)
+	}
+
+	storage.DeleteMailbox(mboxID)
+
+	mbox = storage.GetMailbox(mboxID)
+
+	if mbox != nil {
+		t.Fatalf("Mailbox \"%s\" found after deletion", mboxID)
+	}
+
+	msgCount = storage.CountMessages(mboxID)
+	msgCountExpected = 0
+
+	if msgCount != msgCountExpected {
+		t.Fatalf("Message count is wrong: got %d, expected %d", msgCount, msgCountExpected)
+	}
+}
+
+func TestDeleteMessage(t *testing.T) {
+	var storage = NewStorage()
+
+	var mboxID = "mailbox_1"
+	var msgID_1 = "message_1"
+	var msgID_2 = "message_2"
+
+	for _, ID := range []string{msgID_1, msgID_2} {
+		var msg = storage.GetMessage(ID)
+
+		if msg != nil {
+			t.Errorf("Message \"%s\" found before adding", ID)
+		}
+	}
+
+	storage.Add(&message.Message{ID: msgID_1, ReceivedAt: time.Time{}}, mboxID)
+	storage.Add(&message.Message{ID: msgID_2, ReceivedAt: time.Time{}}, mboxID)
+
+	var mbox = storage.GetMailbox(mboxID)
+
+	if mbox == nil {
+		t.Fatalf("Mailbox \"%s\" not found", mboxID)
+	}
+
+	for _, ID := range []string{msgID_1, msgID_2} {
+		storage.DeleteMessage(ID)
+
+		var msg = storage.GetMessage(ID)
+
+		if msg != nil {
+			t.Errorf("Message \"%s\" found after deletion", ID)
+		}
+	}
+
+	mbox = storage.GetMailbox(mboxID)
+
+	if mbox != nil {
+		t.Fatalf("Mailbox \"%s\" found after deletion of messages", mboxID)
+	}
+}
+
+func TestGetMailbox(t *testing.T) {
+	var storage = NewStorage()
+
+	var mboxID = "mailbox_1"
+
+	storage.Add(&message.Message{ID: "message_1", ReceivedAt: time.Time{}}, mboxID)
+
+	var mbox = storage.GetMailbox(mboxID)
+
+	if mbox == nil {
+		t.Fatalf("Mailbox \"%s\" not found", mboxID)
+	}
+
+	if mbox.ID != mboxID {
+		t.Fatalf("Mailbox ID does not match: got \"%s\", expected \"%s\"", mbox.ID, mboxID)
+	}
+
+	storage.DeleteMailbox(mboxID)
+
+	mbox = storage.GetMailbox(mboxID)
+
+	if mbox != nil {
+		t.Fatalf("Mailbox \"%s\" found after deletion", mboxID)
+	}
+}
+
+func TestGetMailboxes(t *testing.T) {
+	var storage = NewStorage()
+
+	var mboxID_1 = "mailbox_1"
+	var mboxID_2 = "mailbox_2"
+
+	storage.Add(&message.Message{ID: "message_1", ReceivedAt: time.Time{}}, mboxID_1)
+	storage.Add(&message.Message{ID: "message_2", ReceivedAt: time.Time{}}, mboxID_2)
+
+	var mboxCountExpected = 2
+	var mboxCount = storage.CountMailboxes()
+
+	if mboxCount != mboxCountExpected {
+		t.Fatalf("Mailbox count does not match: got %d, expected %d", mboxCount, mboxCountExpected)
+	}
+
+	var mboxes = storage.GetMailboxes()
+
+	if len(mboxes) != mboxCountExpected {
+		t.Fatalf("Mailbox count does not match: got %d, expected %d", len(mboxes), mboxCountExpected)
+	}
+
+	for i, mboxID := range []string{mboxID_1, mboxID_2} {
+		if mboxes[i].ID != mboxID {
+			t.Errorf(
+				"Mailbox ID does not match at index %d: got \"%s\", expected \"%s\"",
+				i,
+				mboxes[i].ID,
+				mboxID,
+			)
+		}
+	}
+}
+
+func TestGetMessages(t *testing.T) {
+	var storage = NewStorage()
+
+	var mboxID = "mailbox_1"
+	var msgID_1 = "message_1"
+	var msgID_2 = "message_2"
+
+	storage.Add(&message.Message{ID: msgID_1, ReceivedAt: time.Time{}}, mboxID)
+	storage.Add(&message.Message{ID: msgID_2, ReceivedAt: time.Time{}}, mboxID)
+
+	var msgCountExpected = 2
+	var msgCount = storage.CountMessages(mboxID)
+
+	if msgCount != msgCountExpected {
+		t.Fatalf("Message count does not match: got %d, expected %d", msgCount, msgCountExpected)
+	}
+
+	var msgs = storage.GetMessages(mboxID)
+
+	if len(msgs) != msgCountExpected {
+		t.Fatalf("Message count does not match: got %d, expected %d", len(msgs), msgCountExpected)
+	}
+
+	for i, msgID := range []string{msgID_2, msgID_1} {
+		if msgs[i].ID != msgID {
+			t.Errorf(
+				"Message ID does not match at index %d: got \"%s\", expected \"%s\"",
+				i,
+				msgs[i].ID,
+				msgID,
+			)
+		}
+	}
+}
